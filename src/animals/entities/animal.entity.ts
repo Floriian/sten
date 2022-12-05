986c76973d16b538a82f3784f668b952a1840dd8
Floriian <lovasifloriian@outlook.hu>
2022-12-05 17:18:06 +0100

    animal entity for openapi

--- src/animals/entities/animal.entity.ts
+++ src/animals/entities/animal.entity.ts
@@ -1,1 +1,16 @@
-export class Animal {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Animal } from '@prisma/client';
+
+export class AnimalEntity implements Animal {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  specie: string;
+
+  @ApiProperty()
+  age: number;
+}

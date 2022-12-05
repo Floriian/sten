37f58233cc5d6219412b5fed7969591db89e07b0
Floriian <lovasifloriian@outlook.hu>
2022-12-05 17:17:58 +0100

    create animal dtos

--- src/animals/dto/create-animal.dto.ts
+++ src/animals/dto/create-animal.dto.ts
@@ -1,1 +1,19 @@
-export class CreateAnimalDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, IsInt } from 'class-validator';
+
+export class CreateAnimalDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  specie: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsInt()
+  age: number;
+}
--- src/animals/dto/update-animal.dto.ts
+++ src/animals/dto/update-animal.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/swagger';
+import { IsInt, IsNotEmpty } from 'class-validator';
 import { CreateAnimalDto } from './create-animal.dto';
 
 export class UpdateAnimalDto extends PartialType(CreateAnimalDto) {}

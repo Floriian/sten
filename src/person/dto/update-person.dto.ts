805de91679c54a8f8add12fef4c5b4a7df9250bb
Floriian <lovasifloriian@outlook.hu>
2022-12-06 20:05:32 +0100

    finishing persons module. tests coming soon....

--- src/person/dto/update-person.dto.ts
+++ src/person/dto/update-person.dto.ts
@@ -1,4 +1,15 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { IsInt, IsOptional } from 'class-validator';
 import { CreatePersonDto } from './create-person.dto';
 
-export class UpdatePersonDto extends PartialType(CreatePersonDto) {}
+export class UpdatePersonDto extends PartialType(CreatePersonDto) {
+  @ApiProperty()
+  @IsOptional()
+  @IsInt()
+  carId: number;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsInt()
+  cityId: number;
+}

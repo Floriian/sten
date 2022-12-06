a59c46ce2e5f589207ffe5c070e742c5db71e33d
Floriian <lovasifloriian@outlook.hu>
2022-12-06 19:36:17 +0100

    create person dto

--- src/person/dto/create-person.dto.ts
+++ src/person/dto/create-person.dto.ts
@@ -1,1 +1,19 @@
-export class CreatePersonDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsInt, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreatePersonDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsInt()
+  age: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsBoolean()
+  sex: boolean;
+}

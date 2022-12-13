94cde582727a9c24891dd45d1beae96144b4794f
Floriian <lovasifloriian@outlook.hu>
2022-12-13 23:26:18 +0100

    fix car params

--- src/car/dto/carparamvalidation.dto.ts
+++ src/car/dto/carparamvalidation.dto.ts
@@ -1,19 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
-import {
-  IsNotEmpty,
-  IsNumberString,
-  IsOptional,
-  IsString,
-} from 'class-validator';
+import { Transform } from 'class-transformer';
+import { IsNumberString, IsOptional, IsString } from 'class-validator';
 
 export class CarParamValiation {
   @IsString()
   @IsOptional()
-  @ApiProperty({ name: 'Licenseplate' })
   licensePlate: string;
 
   @IsNumberString()
   @IsOptional()
-  @ApiProperty({ name: "Car's ID" })
   id: string;
 }
